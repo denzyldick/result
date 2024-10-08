c46359c141ee6a5615574bfaac0bcf78a306164a
Denzyl Dick <2477646+denzyldick@users.noreply.github.com>
2024-10-08 23:21:02 +0200

    Update Purse.php

--- src/Purse.php
+++ src/Purse.php
@@ -10,11 +10,26 @@ namespace Result;
 class Purse
 {
     /**
- *  The concept is simple.
- *  Just put the object int the bag.
- *  And take it out later with the grab method.
- *
- *  This method is returning an enum Result.
+     * The idea of this library is to use the enumerations functionality to 
+     * make it mandatory to handle the different states a return value can have when calling a method. 
+     *
+     * It kind of replaces the try/catch keyword when calling a method. If there are errors you just write it
+     * the error handling in a function and execute it if the state is Err when using the pattern matching. 
+     *
+     * <code>
+     *
+     * /// Passing a bag as a return so that you make it mandatory to handle the error. 
+     * return Purse::put($item);
+     *
+     * /// Calling a method that returns a Result Enum.
+     *  match (method_call()){
+     *   case Result::Ok => other_function(),
+     *   case Result::Error=> handle_error(),
+     * }; 
+     *
+     *
+     * </code>
+     *
      * @param Item $bag
      * @return Result
      */
